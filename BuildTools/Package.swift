1ea4b2b49dc9c5dc9885a51c42343837fcac112d
Gao Sun <gaosun.dev@gmail.com>
2021-06-01 00:32:29 +0800

    Integrate SwiftFormat using SPM

---
+++ BuildTools/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "BuildTools",
+    platforms: [.macOS(.v10_11)],
+    dependencies: [
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.48.4"),
+    ],
+    targets: [.target(name: "BuildTools", path: "")]
+)
